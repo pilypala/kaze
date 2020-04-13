be419e4e47281de8f8be978487249fe947ba31ad
Guoping Huang <pilypala@gmail.com>
2020-04-13 17:56:35 +0800

    basic cmake setup ready

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <QDebug>
+
+using namespace std;
+
+int main()
+{
+    qDebug() << "Hello";
+    return 0;
+
+    // vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    
+    // for (const string& word : msg)
+    // {
+    //     cout << word << " ";
+    // }
+    // cout << endl;
+}
