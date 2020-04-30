0119a05fe7c25478e152a6efb31552e06efd45d1
Guoping Huang <pilypala@gmail.com>
2020-05-01 03:24:23 +0800

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,35 @@
-#include <iostream>
-#include <vector>
-#include <string>
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// #include <QDebug>
+
+// using namespace std;
+
+// int main()
+// {
+//     qDebug() << "Hello";
+//     return 0;
+
+//     // vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    
+//     // for (const string& word : msg)
+//     // {
+//     //     cout << word << " ";
+//     // }
+//     // cout << endl;
+// }
+
+
+#include <QApplication>
+#include <QQmlApplicationEngine>
 #include <QDebug>
 
-using namespace std;
-
-int main()
+int main(int argc, char** argv)
 {
-    qDebug() << "Hello";
+    qDebug() << "Hello World!"; 
     return 0;
-
-    // vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-    
-    // for (const string& word : msg)
-    // {
-    //     cout << word << " ";
-    // }
-    // cout << endl;
+    QApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:///main.qml")));
+    return app.exec();
 }
